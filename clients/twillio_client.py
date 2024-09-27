ba6c70d7e36058105d40612bf43ca6b746f1b8fd
dulikar006 <bhathika19939@gmail.com>
2024-09-27 16:26:07 +0800

    add twillio trigger

--- clients/twillio_client.py
+++ clients/twillio_client.py
@@ -7,13 +7,16 @@ class TwillioClient:
 
     def connect(self):
         account_sid = 'AC7ace3468f7e53936b2c49e16ad354814'
-        auth_token = 'e5ef2d9cfd2925c66f662b9311d3f066'
+        auth_token = '9e6e8059b3a62f7b5ad6d18cdacb7094'
         self.client = Client(account_sid, auth_token)
 
     def send_message(self, message):
-        message = self.client.messages.create(
-          from_='whatsapp:+14155238886',
-          body=message,
-          to='whatsapp:+94772608766' #'whatsapp:+94712986468' 'whatsapp:+6597778562' #
-        )
-        print(message.sid)
+        try:
+            message = self.client.messages.create(
+              from_='whatsapp:+14155238886',
+              body=message,
+              to="whatsapp:+94712986468" #'whatsapp:+94772608766' #'whatsapp:+94712986468' 'whatsapp:+6597778562' #
+            )
+            print(message.sid)
+        except:
+            print("Error sending whatsapp message")
